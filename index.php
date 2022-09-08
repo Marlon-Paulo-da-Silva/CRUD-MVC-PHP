5cb0094993dc3950b09ca051de37bc93cbcdb16c
Marlon Paulo <marlon.paulo.silva@outlook.com>
2022-09-08 17:45:58 -0300

    criando o arquivo da Rotas

--- index.php
+++ index.php
@@ -3,6 +3,13 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use App\Controller\Pages\Home;
+use App\Http\Router;
+
+$obRouter = new Router(''); 
+
+echo '<pre>';
+print_r($obRouter);
+exit;
 
 
 echo Home::getHome();
