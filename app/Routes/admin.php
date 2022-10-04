2e6f9fc0cafe108ef606ce053f116204134ce884
Marlon Paulo <marlon.paulo.silva@outlook.com>
2022-10-04 08:56:57 -0300

    Separando as rotas para organização e criando getHome para o painel inicial

--- app/Routes/admin.php
+++ app/Routes/admin.php
@@ -1,47 +1,12 @@
 <?php
 
-use App\Http\Response;
+// inclui as rotas de Home
+include __DIR__ . '/admin/home.php';
 
-use App\Controller\Admin;
+// inclui as rotas de Login
+include __DIR__ . '/admin/login.php';
 
-// Rota admin
-$obRouter->get('/admin',[
-  'middlewares' => [
-    'require-admin-login'
-  ],
-  function(){
-    return new Response(200, 'Admin :)');
-  }
-]);
 
-// Rota login
-$obRouter->get('/admin/login',[
-  'middlewares' => [
-    'require-admin-logout'
-  ],
-  function($request){
-    return new Response(200, Admin\Login::getLogin($request));
-  }
-]);
 
-// Rota login (POST)
-$obRouter->post('/admin/login',[
-  'middlewares' => [
-    'require-admin-logout'
-  ],
-  function($request){
-    return new Response(200, Admin\Login::setLogin($request));
-  }
-]);
-
-// Rota logout
-$obRouter->get('/admin/logout',[
-  'middlewares' => [
-    'require-admin-login'
-  ],
-  function($request){
-    return new Response(200, Admin\Login::setLogout($request));
-  }
-]);
 
 
