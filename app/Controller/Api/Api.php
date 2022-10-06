7d9f64da91875ec50daea77a1fd0cd09e1092319
Marlon Paulo <marlon.paulo.silva@outlook.com>
2022-10-06 12:52:21 -0300

    Adicionando tipo de retorno JSON

--- app/Controller/Api/Api.php
+++ app/Controller/Api/Api.php
@@ -1,2 +1,16 @@
 <?php
 
+namespace App\Controller\Api;
+
+class Api {
+
+  // Metodo responsável por retornar os detalhes da API
+  public static function getDetails($request){
+    return [
+      'name' => 'API - CRUD MVC',
+      'versão' => 'v1.0.0',
+      'author' => 'Marlon Paulo',
+      'email' => 'marlon.pauloo@gmail.com'
+    ];
+  }
+}
--- app/Routes/api/v1/default.php
+++ app/Routes/api/v1/default.php
@@ -1,10 +1,12 @@
 <?php 
 
 use App\Http\Response;
+use App\Controller\Api;
 
 // Rota admin
 $obRouter->get('/api/v1',[
   function($request){
-    return new Response(200, json_encode(['status' => 'OK']));
+    return new Response(200, Api\Api::getDetails($request), 'application/json');
   }
 ]);
+
